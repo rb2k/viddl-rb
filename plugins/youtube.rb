df503db1fb6e142f8b2ae74f69fba5b8fd46d8a6
Karan Sikka <kssworld93@gmail.com>
2012-01-16 18:43:17 -0500

    made some fixes that should increase compatibility with rails

--- plugins/youtube.rb
+++ plugins/youtube.rb
@@ -1,5 +1,14 @@
-class Youtube < PluginBase
-	#this will be called by the main app to check whether this plugin is responsible for the url passed
+require 'net/http'
+class Youtube
+#some static stuff
+  class << self; attr_reader :registered_plugins end
+    @registered_plugins = []
+
+#if you inherit from this class, the child gets added to the "registered plugins" array
+  def self.inherited(child)
+    PluginBase.registered_plugins << child
+  end	
+#this will be called by the main app to check whether this plugin is responsible for the url passed
 	def self.matches_provider?(url)
 		url.include?("youtube.com") || url.include?("youtu.be")
 	end
@@ -55,7 +64,11 @@ class Youtube < PluginBase
 		end
 		#let's get some infos about the video. data is urlencoded
 		yt_url = "http://www.youtube.com/get_video_info?video_id=#{video_id}"
-		video_info = open(yt_url).read
+		
+    uri_object = URI.parse(yt_url)
+    response =  Net::HTTP.get_response(uri_object)
+    video_info = response.body #open(yt_url).read
+
 		#converting the huge infostring into a hash. simply by splitting it at the & and then splitting it into key and value arround the =
 		#[...]blabla=blubb&narf=poit&marc=awesome[...]
 		video_info_hash = Hash[*video_info.split("&").collect { |v| 
