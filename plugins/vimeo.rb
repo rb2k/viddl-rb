37b0f4cee52f649de512e90bcae52783c4258ba2
Kalle Lindström <lindstrom.kalle@gmail.com>
2012-05-02 23:48:14 +0200

    rewrote plugin, it should pass tests now

--- plugins/vimeo.rb
+++ plugins/vimeo.rb
@@ -1,25 +1,36 @@
-class Vimeo < PluginBase
-	#this will be called by the main app to check whether this plugin is responsible for the url passed
-	def self.matches_provider?(url)
-		url.include?("vimeo.com")
-	end
-	
-	def self.get_urls_and_filenames(url)
-		#the vimeo ID consists of 7 decimal numbers in the URL
-		vimeo_id = url[/\d{7,8}/]
-		doc = Nokogiri::XML(open("http://www.vimeo.com/moogaloop/load/clip:#{vimeo_id}"))
-		title = doc.at("//video/caption").inner_text
-		puts "[VIMEO] Title: #{title}"
-		request_signature = doc.at("//request_signature").inner_text
-		request_signature_expires = doc.at("//request_signature_expires").inner_text
-		
+require 'net/http'
 
-		puts "[VIMEO] Request Signature: #{request_signature} expires: #{request_signature_expires}"
-		
-		download_url = "http://www.vimeo.com/moogaloop/play/clip:#{vimeo_id}/#{request_signature}/#{request_signature_expires}/?q=hd"
-		#todo: put the filename cleaning stuff into a seperate helper
-		file_name = title.delete("\"'").gsub(/[^0-9A-Za-z]/, '_') + ".flv"
-		puts "downloading to " + file_name
-		[{:url => download_url, :name => file_name}]
-	end
+class Vimeo < PluginBase
+  #this will be called by the main app to check whether this plugin is responsible for the url passed
+  def self.matches_provider?(url)
+    url.include?("vimeo.com")
+  end
+  
+  def self.get_urls_and_filenames(url)
+    #the vimeo ID consists of 7 decimal numbers in the URL
+    vimeo_id = url[/\d{7,8}/]
+
+    page_html = open("http://vimeo.com/#{vimeo_id}").read
+
+    title = page_html[/<meta\s+property="og:title"\s+content="(.+?)"/, 1]
+    puts "[VIMEO] Title: #{title}"
+
+    #the timestamp and sig info is in the embedded player javascript in the video page
+    timestamp = page_html[/"timestamp":(\d+),/, 1]
+    signature = page_html[/"signature":"([\d\w]+)",/, 1]
+
+    #send a get request to the redirect url and pass along the id, sig and timestamp info
+    redirect_uri = URI("http://player.vimeo.com/play_redirect?clip_id=#{vimeo_id}&sig=#{signature}&time=#{timestamp}&quality=hd&codecs=H264")
+    redirect_res = Net::HTTP.get_response(redirect_uri)
+
+    #the download url is the value of the location (redirect) header
+    download_url = redirect_res["location"]
+    file_name = make_filename(title)
+
+    [{:url => download_url, :name => file_name}]
+  end
+
+  def self.make_filename(title)
+    title.delete("\"'").gsub(/[^\d\w]/, '_') + ".mp4"
+  end
 end
