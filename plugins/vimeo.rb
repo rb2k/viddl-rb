27aa65dc266df147aa20a63a11e4b46c08011a13
Kalle Lindström <lindstrom.kalle@gmail.com>
2012-05-06 15:49:02 +0200

    use Mechanize

--- plugins/vimeo.rb
+++ plugins/vimeo.rb
@@ -1,4 +1,3 @@
-require 'net/http'
 
 class Vimeo < PluginBase
   #this will be called by the main app to check whether this plugin is responsible for the url passed
@@ -10,7 +9,11 @@ class Vimeo < PluginBase
     #the vimeo ID consists of 7 decimal numbers in the URL
     vimeo_id = url[/\d{7,8}/]
 
-    page_html = open("http://vimeo.com/#{vimeo_id}").read
+    agent = Mechanize.new      #use Mechanize for the automatic cookie handeling
+    agent.redirect_ok = false  #don't follow redirects so we do not download the video when we get it's url
+
+    video_page = agent.get("http://vimeo.com/#{vimeo_id}")
+    page_html = video_page.root.inner_html
 
     title = page_html[/<meta\s+property="og:title"\s+content="(.+?)"/, 1]
     puts "[VIMEO] Title: #{title}"
@@ -19,12 +22,10 @@ class Vimeo < PluginBase
     timestamp = page_html[/"timestamp":(\d+),/, 1]
     signature = page_html[/"signature":"([\d\w]+)",/, 1]
 
-    #send a get request to the redirect url and pass along the id, sig and timestamp info
-    redirect_uri = URI("http://player.vimeo.com/play_redirect?clip_id=#{vimeo_id}&sig=#{signature}&time=#{timestamp}&quality=hd&codecs=H264")
-    redirect_res = Net::HTTP.get_response(redirect_uri)
+    redirect_url = "http://player.vimeo.com/play_redirect?clip_id=#{vimeo_id}&sig=#{signature}&time=#{timestamp}&quality=hd&codecs=H264"
 
     #the download url is the value of the location (redirect) header
-    download_url = redirect_res["location"]
+    download_url = agent.get(redirect_url).header["location"]
     file_name = make_filename(title)
 
     [{:url => download_url, :name => file_name}]
