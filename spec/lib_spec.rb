4dce722d5af4c308951dd88e12da13fd0dbd8393
Kalle Lindström <lindstrom.kalle@gmail.com>
2012-08-29 16:20:26 +0200

    Second try

--- spec/lib_spec.rb
+++ spec/lib_spec.rb
@@ -3,6 +3,7 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
 
 require 'rubygems'
 require 'minitest/autorun'
+require 'rest_client'
 require 'viddl-rb.rb'
 
 class TestURLExtraction < MiniTest::Unit::TestCase
@@ -11,10 +12,11 @@ class TestURLExtraction < MiniTest::Unit::TestCase
   #_________________
 
   def test_can_get_single_youtube_url_and_filename
-    download_urls = ViddlRb.get_urls_and_filenames("http://www.youtube.com/watch?v=QH2-TGUlwu4")
+    download_urls = ViddlRb.get_urls_and_filenames("http://www.youtube.com/watch?v=gZ8w4vVaOL8")
     url = download_urls.first[:url]
 
-    assert_match(/^http:\/\/r13---arn06s03.c.youtube.com\/videoplayback/, url)
+    assert_match(/^http/, url)                            # check that the string start with http
+    assert_match(/c.youtube.com\/videoplayback/, url)     # check that we have the video playback string
   end
 
   # Unit tests
