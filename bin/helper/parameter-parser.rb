cd8ce4328625b9f5dbe0430c95c91f65cf8f8e7b
Kalle Lindström <lindstrom.kalle@gmail.com>
2012-10-05 14:34:00 +0200

    Use OptionParser for parameter parsing

--- bin/helper/parameter-parser.rb
+++ bin/helper/parameter-parser.rb
@@ -1,67 +1,84 @@
 
 # ParameterParser parses the program parameters.
 # If the parameters are not valid in some way an exception is raised.
+# The exceptions raised by this class are handeled in the bin program.
+
 class ParameterParser
 
   DEFAULT_SAVE_DIR = "."
 
   #returns a hash with the parameters in it:
-  # :url            => the video url
-  # :extract_audio  => should attempt to extract audio? (true/false)
-  # :url_only       => do not download, only print the urls to stdout
-  # :title_only     => do not download, only print the titles to stdout
-  # :youtube_filer  => a regular expression used ot fileter youtube playlists
-  # :save_dir       => the directory where the videos are saved
-  def self.parse_app_parameters
-    check_valid_parameters!
+  # :url              => the video url
+  # :extract_audio    => should attempt to extract audio? (true/false)
+  # :url_only         => do not download, only print the urls to stdout
+  # :title_only       => do not download, only print the titles to stdout
+  # :playlist_filter  => a regular expression used to filter playlists
+  # :save_dir         => the directory where the videos are saved
+  def self.parse_app_parameters(args)
 
-    params = {}
-    params[:url] = ARGV.first
-    params[:extract_audio] = ARGV.include?("--extract-audio")
-    params[:url_only] = ARGV.include?("--url-only")
-    params[:title_only] = ARGV.include?("--title-only")
-    params[:playlist_filter] = get_youtube_filter
-    params[:save_dir] = get_save_dir  
-    params
-  end
+    # Default option values are set here
+    options = {
+      :extract_audio    => false,
+      :url_only         => false,
+      :title_only       => false,
+      :playlist_filter  => nil,
+      :save_dir         => DEFAULT_SAVE_DIR
+    }
 
-  #check if parameters are valid.
-  #the exceptions raised by this method are caught by the viddl-rb bin utility.
-  def self.check_valid_parameters!
-    if ARGV.empty?
-      raise "Usage: viddl-rb URL [--extract-audio]"
-    elsif !ARGV.first.match(/^http/)
-      raise "ERROR: Please include 'http' with your URL e.g. http://www.youtube.com/watch?v=QH2-TGUlwu4"
-    elsif ARGV.include?("--extract-audio") && !DownloadHelper.os_has?("ffmpeg")
-      raise "ERROR: To extract audio you need to have ffmpeg on your system"
+    optparse = OptionParser.new do |opts|
+      opts.banner = "Usage: viddl-rb URL [options]"
+
+      opts.on("-e", "--extract-audio", "Save video audio to file") do
+        if ViddlRb::UtilityHelper.os_has?("ffmpeg")
+          options[:extract_audio] = true
+        else
+          raise OptionParser::ParseError.new("to extract audio you need to have ffmpeg on your PATH")
+        end
+      end
+
+      opts.on("-u", "--url-only", "Prints url without downloading") do
+        options[:url_only] = true
+      end
+
+      opts.on("-t", "--title-only", "Prints title without downloading") do
+        options[:title_only] = true
+      end
+
+      opts.on("-f", "--filter REGEX", Regexp, "Filters a video playlist according to the regex") do |regex|
+        options[:filter] = regex
+      end
+
+      opts.on("-s", "--save-dir DIRECTORY", "Specifies the directory where videos should be saved") do |dir|
+        if File.directory?(dir)
+          options[:save_dir] = dir
+        else
+          raise OptionParser::InvalidArgument.new("'#{dir}' is not a valid directory")
+        end
+      end
+
+      opts.on_tail('-h', '--help', 'Display this screen') do
+        print_help_and_exit(opts)
+      end
     end
+
+    optparse.parse!(args)                           # removes all options from args
+    print_help_and_exit(optparse) if args.empty?    # exit if no video url
+    url = validate_url(args.first)                  # the url is the only element left
+    options[:url] = url                     
+    options
   end
 
-  #gets the regular expression used to filter youtube playlists.
-  def self.get_youtube_filter
-    filter = ARGV.find { |arg| arg =~ /--filter=./ }  # --filter= and at least one more character
-    return nil unless filter
-
-    ignore_case = filter.include?("/i")
-    regex = filter[/--filter=(.*?)(?:\/|$)/, 1]   # everything up to the first / (could be an empty string)
-    raise "ERROR: '#{regex}' is not a valid regular expression" unless is_valid_regex?(regex)
-    Regexp.new(regex, ignore_case)
+  def self.print_help_and_exit(opts)
+    puts opts
+    exit(0)
   end
 
-  #checks if the string is a valid regex (for example "*****" is not)
-  def self.is_valid_regex?(regex)
-    Regexp.compile(regex)
-  rescue RegexpError
-    false
-  end
-  
-  #gets the directory used for saving videos in.
-  def self.get_save_dir
-    save_dir = ARGV.find { |arg| arg =~ /--save-dir=./ }
-    return DEFAULT_SAVE_DIR unless save_dir
-
-    dir = save_dir[/--save-dir=(.+)/, 1]
-    raise "ERROR: '#{dir}' is not a valid directory or does not exist" unless File.directory?(dir)
-    dir
+  def self.validate_url(url)
+    if url =~ /^http/
+      url
+    else
+      raise OptionParser::InvalidArgument.new(
+        "please include 'http' with your URL e.g. http://www.youtube.com/watch?v=QH2-TGUlwu4")
+    end
   end
 end
