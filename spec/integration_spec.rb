50f37442fe2aaa560e3c5d4eb55a171308db1aa7
Kalle Lindström <lindstrom.kalle@gmail.com>
2012-10-05 18:16:44 +0200

    Added audio extraction to integration spec

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -14,13 +14,16 @@ class TestURLExtraction < MiniTest::Unit::TestCase
   
   private
   
+  #Test video download and audio extraction
   def download_test(url)
     before = Dir['*']
-    assert system("ruby bin/viddl-rb #{url}")
-    new_file = Dir['*'] - before
-    assert_equal new_file.size, 1
-    assert File.size(new_file[0]) > 100000
-    File.unlink(new_file[0])
+    assert system("ruby bin/viddl-rb #{url} -e")
+    new_files = Dir['*'] - before
+    assert_equal new_files.size, 2
+    assert File.size(new_files[0]) > 100000
+    assert File.size(new_files[1]) > 40000
+    File.unlink(new_files[0])
+    File.unlink(new_files[1])
   end
   
 end
