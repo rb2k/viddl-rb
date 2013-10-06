ba8f30c0329dcadb5e95f4cd6b6f536144b0f758
Kalle Lindström <lindstrom.kalle@gmail.com>
2013-10-06 13:37:13 +0200

    Added redirection following

--- plugins/soundcloud.rb
+++ plugins/soundcloud.rb
@@ -7,12 +7,13 @@ class Soundcloud < PluginBase
 
   # return the url for original video file and title
   def self.get_urls_and_filenames(url, options = {})
-    doc          = Nokogiri::HTML(open(get_http_url(url)))
+    doc               = Nokogiri::HTML(open(get_http_url(url)))
     download_filename = doc.at("#main-content-inner img[class=waveform]").attributes["src"].value.to_s.match(/\.com\/(.+)\_/)[1]
-    download_url = "http://media.soundcloud.com/stream/#{download_filename}"
-    file_name    = transliterate("#{doc.at('//h1/em').text.chomp}") + ".mp3"
+    download_url      = "http://media.soundcloud.com/stream/#{download_filename}"
+    final_url         = UtilityHelper.get_final_location(download_url)
+    file_name         = transliterate("#{doc.at('//h1/em').text.chomp}") + ".mp3"
 
-    [{:url => download_url, :name => file_name}]
+    [{:url => final_url, :name => file_name}]
   end
 
   def self.transliterate(str)
