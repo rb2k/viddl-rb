390d170a85ffa67a262ce956f76b8a7b540a7861
Marc Seeger <mail@marc-seeger.de>
2015-02-01 18:22:48 -0500

    Update soundcloud plugin. Fixes #127

--- plugins/soundcloud.rb
+++ plugins/soundcloud.rb
@@ -9,39 +9,23 @@ class Soundcloud < PluginBase
   end
 
 
-  # return the url for original video file and title
+  # return the url for original audio file and title
   def self.get_urls_and_filenames(url, options = {})
     url_and_files = []
 
-    client = RestClient::Resource.new(get_http_url(url), :verify_ssl => OpenSSL::SSL::VERIFY_NONE)
-    doc = Nokogiri::HTML(client.get)
-    js = doc.at("script:contains('_scPreload')").text
-
-    track_data = JSON.parse js[/_scPreload\s*?=\s*?([\s\S]*?)$/,1].to_s
-
-    track_data['data']['models/audible'].each do |track_data|
-      params    = URI.encode_www_form 'app_version' => '6749d1a0',
-                                      'client_id'   => 'b45b1aa10f1ac2941910a7f0d10f8e28',
-                                      'policy'      => 'ALLOW'
-
-      url_and_files << {
-          url:  URI("#{track_data['stream_url']}?#{params}").to_s.to_s,
-          name: self.make_filename_safe(track_data['title'].to_s) + '.mp3'
-      }
-    end
-
-
-    # locate the controller script that contains all the tracks data
-    # this will work for either artist's or track's pages
-    doc.css('#main-content-inner .container + script').each do |container|
-      match       = container.text.to_s.match(/\((\{.*\})\)/).to_a
-      track_data  = JSON.parse(match[1])
-      
-      file_url    = track_data['streamUrl']
-      file_name   = self.make_filename_safe(track_data['title'].to_s) + '.mp3'
-
-      url_and_files << {url: file_url, name: file_name}
-    end
+    track_id_url = "https://api.sndcdn.com/resolve?url=#{URI::encode(url)}&client_id=b45b1aa10f1ac2941910a7f0d10f8e28"
+    track_id_response = open(track_id_url, {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}).read
+    track_id_response_parsed = JSON.parse(track_id_response)
+    title = track_id_response_parsed['title']
+    track_id = track_id_response_parsed['id']
+    download_metadata_url = "https://api.sndcdn.com/i1/tracks/#{track_id}/streams?client_id=b45b1aa10f1ac2941910a7f0d10f8e28"
+    download_metadata_response = open(download_metadata_url, {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}).read
+    download_url = JSON.parse(download_metadata_response)['http_mp3_128_url']
+    
+    url_and_files << {
+          url:  download_url,
+          name: self.make_filename_safe(title) + '.mp3'
+    }
 
     url_and_files
   end
